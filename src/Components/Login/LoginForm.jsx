830c22e2dade35c12419e112f0b5ef36290b8b13
Gabriel Silva <gabrielp_lacerda@hotmail.com>
2023-07-11 17:36:10 -0300

    validação do formulário

---
+++ src/Components/Login/LoginForm.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
+
+const LoginForm = () => {
+  const username = useForm();
+  const password = useForm();
+  console.log(password.value);
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (username.validate() && password.validate()) {
+      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(),
+      })
+        .then((response) => {
+          console.log(response);
+          return response;
+        })
+        .then((json) => {
+          console.log(json);
+          return json;
+        });
+    }
+  }
+
+  return (
+    <section>
+      <h1>Titulo</h1>
+      <form action="#" onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Entrar</Button>
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </section>
+  );
+};
+
+export default LoginForm;
